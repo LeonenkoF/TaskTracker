6d53f04acafca915f66a5712902bad47e2c17051
Fedor.L <leonenko22222@mail.ru>
2023-01-28 16:49:07 +0500

    feat: добавить основные классы.

---
+++ src/TaskData/Epic.java
@@ -0,0 +1,4 @@
+package TaskData;
+
+public class Epic {
+}
---
+++ src/TaskData/Task.java
@@ -0,0 +1,10 @@
+package TaskData;
+
+public class Task {
+
+    private String taskName;
+    private String description;
+    private int id;
+    private String status;
+
+}
