4554483ae1adaf9ad2d8c2af4724063ba8ab3746
Fedor.L <leonenko22222@mail.ru>
2023-01-29 13:50:23 +0500

    feat: реализовал метод обновления задач

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,19 @@ public class Main {
 
         storage.createEpic("Епик 1","Описание Епик 1","NEW",1);
 
-
-        System.out.println(storage.getEpicsMap());
-
         storage.createSubtask("Подзадача 1","Описание подзадачи 1","NEW",1,1);
         storage.createSubtask("Подзадача 2","Описание подзадачи 2","NEW",2,1);
         storage.createSubtask("Подзадача 3","Описание подзадачи 3","NEW",3,1);
 
-        System.out.println((storage.getEpicsMap()));
+        System.out.println(storage.getEpicsMap());
 
+        storage.updateEpic("Изменёный эпик 1","Новое Описание Епик 1","NEW",1);
 
+        System.out.println(storage.getEpicsMap());
+
+        storage.updateSubtask("Новая Подзадача 3","Новое Описание подзадачи 3","NEW",3,1);
+
+        System.out.println(storage.getEpicsMap());
 
     }
 }
--- src/TaskData/Storage.java
+++ src/TaskData/Storage.java
@@ -1,6 +1,7 @@
 package TaskData;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Storage {
     HashMap<Integer,Task> tasksMap = new HashMap<>();
@@ -37,6 +38,44 @@ public class Storage {
     public void createSubtask(String taskName, String description, String status, Integer id,Integer epicId){
         Subtask newSubtask = new Subtask(taskName,description,status,id,epicId);
         epicsMap.get(epicId).subtasks.put(id,newSubtask);
+    }
+
+    public  void deleteSubtaskById(Integer epicId,Integer id){
+        epicsMap.get(epicId).subtasks.remove(id);
+    }
+
+    public void deleteAllTasks() {
+        tasksMap.clear();
+    }
+
+    public void deleteAllEpics() {
+        epicsMap.clear();
+    }
+
+    public Object getSubtasksOfEpic(Integer epicId) {
+        return epicsMap.get(epicId).subtasks.values();
+    }
+
+    public void updateTask(String taskName, String description,String status,Integer id) {
+        Task newTask = new Task(taskName,description,status,id);
+        tasksMap.replace(id, newTask);
+    }
+
+    public void updateEpic(String taskName, String description,String status,Integer id) {
+        Epic rewriteEpic;
+        rewriteEpic = epicsMap.get(id);
+        rewriteEpic.taskName = taskName;
+        rewriteEpic.description = description;
+        rewriteEpic.status = status;
+        rewriteEpic.id = id;
+        epicsMap.replace(id,rewriteEpic);
+    }
+
+    public void updateSubtask(String taskName, String description, String status, Integer id,Integer epicId) {
+        Subtask newSubtask = new Subtask(taskName,description,status,id,epicId);
+        epicsMap.get(epicId).subtasks.replace(id,newSubtask);
+
+
     }
 }
 
